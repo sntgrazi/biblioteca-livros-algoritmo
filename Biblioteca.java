d27be1f64701c7c908015c2420b6c54c4875cbce
Grazi Santana <99621932+sntgrazi@users.noreply.github.com>
2024-02-23 21:10:03 -0300

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -47,6 +47,3 @@ public class Biblioteca {
     }
 }
 
-/* Concluindo:  No contexto de uma biblioteca, onde a busca por ID é uma operação comum e a associação eficiente de informações, como livros, a chaves únicas é crucial, 
-a escolha do HashMap é bastante eficiente. Essa estrutura de dados oferece um equilíbrio entre eficiência e simplicidade de implementação para atender aos requisitos apresentados. 
-Com o HashMap, otimizamos a recuperação de informações, garantindo um acesso rápido e direto aos dados relevantes. */
